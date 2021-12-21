87fffb9c60ff361e79335c2325b5ca18b1634b6f
Jack Transue <johnktransue@gmail.com>
2021-12-20 23:53:25 -0500

    very close to initial finish

--- pages/index.tsx
+++ pages/index.tsx
@@ -24,10 +24,7 @@ interface Item {
 const Home: NextPage<Props> = () => {
   const [jobs, setJobs] = useState<Item[]>([]);
   const [department, setDepartment] = useState<Item[]>([]);
-  const [selection, setSelection] = useState<Item[]>([]);
-  const [deptSelect, setdeptSelect] = useState<Item[]>([]);
-  const [checked, setChecked] = useState(false);
-  const [clicked, setClicked] = useState<Item[]>([]);
+  const [selection, setSelection] = useState(null);
 
   useEffect(() => {
     axios({
@@ -48,21 +45,9 @@ const Home: NextPage<Props> = () => {
 
   console.log(jobs);
 
-  // const newYork = jobs.filter((item) => item.location.name === "Sofia");
-
-  //  const dataFilter = jobs.filter((item) => item.departments[0].name === "Data");
-
   let deptFilter = jobs.filter(
     (item) => item.departments[0].name === selection
   );
-
-  let deptChecks = department.filter(
-    (item) => item.id === deptSelect
-  );
-  // console.log(deptFilter);
-
-  // console.log(newYork);
-  // console.log(dataFilter);
 
   //write function for if department on left is selected equals jobs.data.departments, filter
 
@@ -74,84 +59,54 @@ const Home: NextPage<Props> = () => {
           return <li key={index}>{item.title}</li>;
         })}
       </div> */}
-      <div className="flex flex-row">
-        <div>
-          {department.map((item, index) => {
-            return (
-              <>
-                <ul>
-                  <form>
-                    {/* {console.log("selection-id", selection)} */}
-                    <li key={index} className={checked ? `text-blue-400` : `text-gray-400`}>
-                      {/* <button
-                        id={`checkbox-${item.index}`}
-                        onClick={() => setClicked(item.id)}
-                      > */}
-                        {/* {console.log(clicked)} */}
-                        <input
-                          type="checkbox"
-                          onClick={() => setSelection(item.name)}
-                          
-                          checked={checked}
-                          onChange={e => ((e.target.id = item.id ? setChecked(e.target.checked) : null))}
-                          id={item.id}
-                          className="check-radio bg-blue text-blue"
-                        />
+      <div className="flex lg:flex-row flex-col">
+        <div className="lg:m-20 p-5 lg:w-96">
+          <h1 className="inter text-base mb-4">Departments</h1>
+          <ul>
+            {department.map((item, index) => {
+              return (
+                <>
+                  {/* {console.log("selection-id", selection)} */}
+                  <li key={index}>
+                    <label
+                      for={item.id}
+                      className="check-container center-radio"
+                    >
+                      <input
+                        type="radio"
+                        name={`selection`}
+                        onClick={() => setSelection(item.name)}
+                        id={item.id}
+                        className="check-radio bg-blue text-blue"
+                      />
+                      <span className="checkmark"></span>
+                      <span className="lg:whitespace-nowrap text-sm relative bt-2px">
                         {item.name}
-                      {/* </button> */}
-                    </li>
-                  </form>
-                </ul>
-              </>
-            );
-          })}
+                      </span>
+                    </label>
+                  </li>
+                </>
+              );
+            })}
+          </ul>
         </div>
-        <div>
+        <div className="my-20 lg:mr-32 view-width">
           {/* {jobs.filter((item) => item.departments[0].name === selection)} */}
-          {selection === null ? (
+          {selection == null ? (
             <>
-              {jobs.map((item, index) => {
+              {/* {jobs.map((item, index) => {
                 // console.log(jobs);
                 return <li key={index}>{item.title}</li>;
-              })}
+              })} */}
+              <AccordionPage data={jobs} />
             </>
           ) : (
             <>
-              {/* {deptFilter.map((item, index) => {
-                // console.log(jobs);
-                return (
-                  <li key={index}>
-                    <div>
-                        <h1 className="text-2xl">{item.title}</h1>
-                        <h2 className="text-xl">{item.departments[0].name}</h2>
-                        <p className="text-sm">{parse(decode(item.content))}</p>
-                      <a
-                        target="_blank"
-                        href={item.absolute_url}
-                        alt={`${item.title} job application link`}
-                      >
-                        Apply Now
-                      </a>
-                        <h5>{item.location.name}</h5>
-                    </div>
-                  </li>
-                );
-              })} */}
               <AccordionPage data={deptFilter} />
             </>
           )}
-          {/* {jobs.map((item, index) => {
-            // console.log(jobs);
-            return <li key={index}>{item.title}</li>;
-          })}
-          {
-            deptFilter.map((item, index) => {
-            // console.log(jobs);
-            return <li key={index}>{item.title}</li>;
-          })} */}
         </div>
       </div>
-      {/* <AccordionPage data={jobs} /> */}
     </div>
   );
 };
